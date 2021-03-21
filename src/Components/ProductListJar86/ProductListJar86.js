9d93d5b26816ca37df35a88decd0c0153b08c9a5
Jhonatan Araneda <jharanedac@gmail.com>
2021-03-20 22:11:27 -0700

    Added ProductList component which display each item into the inventory

---
+++ src/Components/ProductListJar86/ProductListJar86.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const ProductListJar86 = ({ inventory, addAction }) => {
+  return (
+    <div className="row border-dark border rounded float-start">
+      <div className="container col-sm-12 col-md-12 border-danger rounded">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>SKU</th>
+              <th>Name</th>
+              <th>Quantity</th>
+              <th>Price</th>
+              <th>Add</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr><td>
+                </td>
+              </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ProductListJar86;
