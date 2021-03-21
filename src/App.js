19ed5d588c85cd14a454593896fc937a571d4486
Jhonatan Araneda <jharanedac@gmail.com>
2021-03-21 14:03:37 -0700

    Update App file, Added import routes. Added states, useEffect and refreshPage function which will get/set the information from WS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
-
+import ProductListJar86 from "./Components/ProductListJar86/ProductListJar86";
+import CartJar86 from "./Components/CartJar86/CartJar86";
+import HeaderJar86 from "./Components/HeaderJar86/HeaderJar86";
+import { useState, useEffect } from "react";
+import {
+  getCartJar86,
+  addItemJar86,
+  removeItemJar86,
+  checkout,
+  getInventory,
+  getTotals,
+} from "./Components/ServicesJar86/inventoryServiceJar86";
 
 function App() {
-  return (
-    <div className="App">
-      algo
-    </div>
-  );
+  const [inventory, setInventory] = useState([]);
+  const [loading, setLoading] = useState();
+  const [error, setError] = useState();
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState({
+    total: 0,
+  });
+
+  useEffect(() => {
+    refreshPage();
+  }, []);
+
+  async function refreshPage() {
+    setLoading(true);
+
+    getCartJar86()
+      .then((json) => {
+        setCart(json);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setError(err);
+      });
+
+    await getInventory()
+      .then((json) => {
+        setInventory(json);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setError(err);
+      });
+
+    await getTotals()
+      .then((json) => {
+        setTotal(json);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setError(err);
+      });
+  }
+
+  return <div className="App">algo</div>;
 }
 
 export default App;
