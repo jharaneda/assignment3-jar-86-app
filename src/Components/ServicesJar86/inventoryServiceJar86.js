948b850a17bb50c98516e92fbffa28102ef3f0f5
Jhonatan Araneda <jharanedac@gmail.com>
2021-03-21 11:24:04 -0700

    Update inventoryService file, added getTotals function which will get the cart total price

--- src/Components/ServicesJar86/inventoryServiceJar86.js
+++ src/Components/ServicesJar86/inventoryServiceJar86.js
@@ -4,33 +4,40 @@ export async function getCartJar86() {
     response.json()
   );
 }
+
 export async function getInventory(newItem) {
   return fetch("http://localhost:8000/inventoryJar86").then((response) =>
     response.json()
   );
 }
 
+export async function getTotals() {
+  return fetch("http://localhost:8000/cartJar86/totals").then((response) =>
+    response.json()
+  );
+}
+
 export async function addItemJar86(newItem) {
-    const itemData = JSON.stringify(newItem);
-    return fetch("http://localhost:8000/cartJar86", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: itemData,
-    }).then((response) => response.json());
-  }
+  const itemData = JSON.stringify(newItem);
+  return fetch("http://localhost:8000/cartJar86", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: itemData,
+  }).then((response) => response.json());
+}
 
-  export async function removeItemJar86(deleteItem) {
-    const itemData = JSON.stringify(deleteItem);
-    return fetch("http://localhost:8000/cartJar86", {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
-      body: itemData,
-    }).then((response) => response.json());
-  }
+export async function removeItemJar86(deleteItem) {
+  const itemData = JSON.stringify(deleteItem);
+  return fetch("http://localhost:8000/cartJar86", {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+    body: itemData,
+  }).then((response) => response.json());
+}
 
-  export async function checkout() {
-    return fetch("http://localhost:8000/cartJar86/checkout", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-    }).then((response) => response.json());
-  }
+export async function checkout() {
+  return fetch("http://localhost:8000/cartJar86/checkout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  }).then((response) => response.json());
+}
