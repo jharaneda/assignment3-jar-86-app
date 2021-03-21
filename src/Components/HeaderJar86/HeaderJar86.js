4b83b6153c5c2bd0d95038c8f0b96a620fce65fe
Jhonatan Araneda <jharanedac@gmail.com>
2021-03-20 22:07:26 -0700

    Create components folder and add Header component

---
+++ src/Components/HeaderJar86/HeaderJar86.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HeaderJar86 = () => {
+  return <h1 style={{backgroundColor: "#6C757D", color: 'white', margin: '10px'}}>Assignment #3 - Jhonatan Araneda (300321686)</h1>;
+};
+
+export default HeaderJar86;
