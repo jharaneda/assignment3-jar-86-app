0b749cbcef6a19b45a1fc6aed5653f828c403f47
Jhonatan Araneda <jharanedac@gmail.com>
2021-03-21 17:25:24 -0700

    Update InventoryService file, update URL to talk to AWS

--- src/Components/ServicesJar86/inventoryServiceJar86.js
+++ src/Components/ServicesJar86/inventoryServiceJar86.js
@@ -1,25 +1,30 @@
 // Service file to put all our Fetch calls
 export async function getCartJar86() {
-  return fetch("http://localhost:8000/cartJar86").then((response) =>
+    // 35.153.66.33
+//   return fetch("http://localhost:8000/cartJar86").then((response) =>
+return fetch("http://35.153.66.33:8000/cartJar86").then((response) =>
     response.json()
   );
 }
 
 export async function getInventory(newItem) {
-  return fetch("http://localhost:8000/inventoryJar86").then((response) =>
+//   return fetch("http://localhost:8000/inventoryJar86").then((response) =>
+return fetch("http://35.153.66.33:8000/cartJar86").then((response) =>
     response.json()
   );
 }
 
 export async function getTotals() {
-  return fetch("http://localhost:8000/cartJar86/totals").then((response) =>
+//   return fetch("http://localhost:8000/cartJar86/totals").then((response) =>
+return fetch("http://35.153.66.33:8000/cartJar86").then((response) =>
     response.json()
   );
 }
 
 export async function addItemJar86(newItem) {
   const itemData = JSON.stringify(newItem);
-  return fetch("http://localhost:8000/cartJar86", {
+//   return fetch("http://localhost:8000/cartJar86", {
+    return fetch("http://35.153.66.33:8000/cartJar86", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: itemData,
@@ -28,7 +33,8 @@ export async function addItemJar86(newItem) {
 
 export async function removeItemJar86(deleteItem) {
   const itemData = JSON.stringify(deleteItem);
-  return fetch("http://localhost:8000/cartJar86", {
+//   return fetch("http://localhost:8000/cartJar86", {
+    return fetch("http://35.153.66.33:8000/cartJar86", {
     method: "DELETE",
     headers: { "Content-Type": "application/json" },
     body: itemData,
@@ -37,6 +43,7 @@ export async function removeItemJar86(deleteItem) {
 
 export async function checkout() {
   return fetch("http://localhost:8000/cartJar86/checkout", {
+    return fetch("http://localhost:8000/cartJar86/checkout", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
   }).then((response) => response.json());
