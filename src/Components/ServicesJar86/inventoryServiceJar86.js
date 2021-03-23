f267f1dfc8a9df69d62abfa6455e8a3972c8865e
Jhonatan Araneda <jharanedac@gmail.com>
2021-03-22 21:34:44 -0700

    update URL AWS

--- src/Components/ServicesJar86/inventoryServiceJar86.js
+++ src/Components/ServicesJar86/inventoryServiceJar86.js
@@ -1,25 +1,25 @@
 // Service file to put all our Fetch calls with different routes 
 export async function getCartJar86() {
-return fetch("http://100.25.37.20:8000/cartJar86").then((response) =>
+return fetch("http://34.229.56.23:8000/cartJar86").then((response) =>
     response.json()
   );
 }
 
 export async function getInventory(newItem) {
-return fetch("http://100.25.37.20:8000/inventoryJar86").then((response) =>
+return fetch("http://34.229.56.23:8000/inventoryJar86").then((response) =>
     response.json()
   );
 }
 
 export async function getTotals() {
-return fetch("http://100.25.37.20:8000/cartJar86/totals").then((response) =>
+return fetch("http://34.229.56.23:8000/cartJar86/totals").then((response) =>
     response.json()
   );
 }
 
 export async function addItemJar86(newItem) {
   const itemData = JSON.stringify(newItem);
-    return fetch("http://100.25.37.20:8000/cartJar86", {
+    return fetch("http://34.229.56.23:8000/cartJar86", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: itemData,
@@ -28,7 +28,7 @@ export async function addItemJar86(newItem) {
 
 export async function removeItemJar86(deleteItem) {
   const itemData = JSON.stringify(deleteItem);
-    return fetch("http://100.25.37.20:8000/cartJar86", {
+    return fetch("http://34.229.56.23:8000/cartJar86", {
     method: "DELETE",
     headers: { "Content-Type": "application/json" },
     body: itemData,
@@ -36,7 +36,7 @@ export async function removeItemJar86(deleteItem) {
 }
 
 export async function checkout() {
-    return fetch("http://100.25.37.20:8000/cartJar86/checkout", {
+    return fetch("http://34.229.56.23:8000/cartJar86/checkout", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
   }).then((response) => response.json());
